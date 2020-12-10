fe140c0fa0cbf45ce487bb72c81fe45c999e13ee
juniorribeiro89 <57049828+juniorribeiro89@users.noreply.github.com>
2020-12-10 20:16:09 -0300

    Segunda Avaliação
    10/12/2020 Entrega do Projeto Agenda de Contatos.

---
+++ agenda-contato4/src/main/java/br/com/acme/agenda/utils/Constantes.java
@@ -0,0 +1,15 @@
+package br.com.acme.agenda.utils;
+
+public class Constantes {
+
+	public static final String PERSISTENCE_UNIT_NAME = "ACME";
+	
+	/**
+	 * CONTATOS
+	 */
+	
+	public static final String LISTAR_CONTATOS = "/pages/cadastros/contatos/lista_contatos.jsp";
+	public static final String CADASTRAR_CONTATOS = "/pages/cadastros/contatos/cadastro_contatos.jsp";
+	public static final String HOME = "/admin/dashboard/index.jsp";
+	public static final String LOGIN = "/login.jsp";
+}
